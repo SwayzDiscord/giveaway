b72100ae0f2d198d425d18551fbdc42a3f00441d
CxtrollerFN <77362066+CxtrollerFN@users.noreply.github.com>
2021-06-02 08:05:44 -0400

    Update ready.js

--- events/discord/ready.js
+++ events/discord/ready.js
@@ -1,9 +1,9 @@
 module.exports = (client) => {
   console.log(`[STATUS] ${client.user.tag} is now online!\n[INFO] Bot by ZeroSync https://www.youtube.com/c/ZeroSync\n[Invite Link] https://discord.com/oauth2/authorize?client_id=${client.user.id}&scope=bot&permissions=8\n[INFO] Ready to serve in ${client.channels.cache.size} channels on ${client.guilds.cache.size} servers, for a total of ${client.users.cache.size} users.`);
-  const activities = [`All Giveaways Running Smoothly`, `ZeroSync on YouTube`];
+  const activities = [``,`g!help`];
   setInterval(() => {
     let activity = activities[Math.floor(Math.random() * activities.length)];
-    client.user.setActivity(activity, { type: "WATCHING" });
+    client.user.setActivity(activity, { type: "STREAMING" });
   }, 20000);
 
 };
